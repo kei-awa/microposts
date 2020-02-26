cca1f3d22b5bc9de9d51705e3c49e1cbcdba01c6
kei.awa <ogita2siam.git@gmail.com>
2020-02-26 10:41:46 +0000

    follow

--- app/User.php
+++ app/User.php
@@ -31,4 +31,44 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
+    public function followings() 
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_id', 'follow_id', 'user_follow')->withTimestamps();
+    }
+    
+    public function follow()
+    {
+        $exist = $this->is_following($userId);
+        
+        $its_me = $this->id == $userId;
+        
+        if ($exist || $its_me) {
+            return false;
+        } else {
+            $this->followings()->attach($userId);
+            return true;
+        }
+        
+    }
+    public function unfollow()
+    {
+        $exists = $thisa->is_following($useId);
+        
+        $its_me =$this->id == $userId;
+        
+        if ($exists && $its_me) {
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function is_following()
+    {
+        return $this->followings()->where('follow_id', 'user_id')->exists();
+    }
 }
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -7,7 +7,7 @@
                 {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
             </div>
             <div>
-                <p class="mb-0">{!!  nl2br(e($micropost->content)) !!}</p>
+                <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
             </div>
             <div>
                 @if (Auth::id()  == $micropost->user_id)
